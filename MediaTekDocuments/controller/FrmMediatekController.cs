eeb80674616daa3efcacc9a227a3ebb9a6d80851
fedizz4 <fedizran41@gmail.com>
2023-06-16 12:35:37 +0200

    Mission 1 : tache 1

---
+++ MediaTekDocuments/controller/FrmMediatekController.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections.Generic;
+using MediaTekDocuments.model;
+using MediaTekDocuments.dal;
+
+namespace MediaTekDocuments.controller
+{
+    /// <summary>
+    /// Contrôleur lié à FrmMediatek
+    /// </summary>
+    class FrmMediatekController
+    {
+        /// <summary>
+        /// Objet d'accès aux données
+        /// </summary>
+        private readonly Access access;
+
+        /// <summary>
+        /// Récupération de l'instance unique d'accès aux données
+        /// </summary>
+        public FrmMediatekController()
+        {
+            access = Access.GetInstance();
+        }
+
+        /// <summary>
+        /// getter sur la liste des genres
+        /// </summary>
+        /// <returns>Liste d'objets Genre</returns>
+        public List<Categorie> GetAllGenres()
+        {
+            return access.GetAllGenres();
+        }
+
+        /// <summary>
+        /// getter sur la liste des livres
+        /// </summary>
+        /// <returns>Liste d'objets Livre</returns>
+        public List<Livre> GetAllLivres()
+        {
+            return access.GetAllLivres();
+        }
+
+        /// <summary>
+        /// getter sur la liste des Dvd
+        /// </summary>
+        /// <returns>Liste d'objets dvd</returns>
+        public List<Dvd> GetAllDvd()
+        {
+            return access.GetAllDvd();
+        }
+
+        /// <summary>
+        /// getter sur la liste des revues
+        /// </summary>
+        /// <returns>Liste d'objets Revue</returns>
+        public List<Revue> GetAllRevues()
+        {
+            return access.GetAllRevues();
+        }
+
+        /// <summary>
+        /// getter sur les rayons
+        /// </summary>
+        /// <returns>Liste d'objets Rayon</returns>
+        public List<Categorie> GetAllRayons()
+        {
+            return access.GetAllRayons();
+        }
+
+        /// <summary>
+        /// getter sur les publics
+        /// </summary>
+        /// <returns>Liste d'objets Public</returns>
+        public List<Categorie> GetAllPublics()
+        {
+            return access.GetAllPublics();
+        }
+
+
+        /// <summary>
+        /// récupère les exemplaires d'une revue
+        /// </summary>
+        /// <param name="idDocuement">id de la revue concernée</param>
+        /// <returns>Liste d'objets Exemplaire</returns>
+        public List<Exemplaire> GetExemplairesRevue(string idDocuement)
+        {
+            return access.GetExemplairesRevue(idDocuement);
+        }
+
+        /// <summary>
+        /// Crée un exemplaire d'une revue dans la bdd
+        /// </summary>
+        /// <param name="exemplaire">L'objet Exemplaire concerné</param>
+        /// <returns>True si la création a pu se faire</returns>
+        public bool CreerExemplaire(Exemplaire exemplaire)
+        {
+            return access.CreerExemplaire(exemplaire);
+        }
+
+        /// <summary>
+        /// Crée un document dans la bdd
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <param name="Titre"></param>
+        /// <param name="Image"></param>
+        /// <param name="IdGenre"></param>
+        /// <param name="IdPublic"></param>
+        /// <param name="IdRayon"></param>
+        /// <returns></returns>
+        public bool CreerDocument(string Id, string Titre, string Image, string IdRayon, string IdPublic, string IdGenre)
+        {
+            return access.CreerDocument(Id, Titre, Image, IdRayon, IdPublic, IdGenre);
+        }
+
+        /// <summary>
+        /// Modifie un document dans la bdd
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <param name="Titre"></param>
+        /// <param name="Image"></param>
+        /// <param name="IdGenre"></param>
+        /// <param name="IdPublic"></param>
+        /// <param name="IdRayon"></param>
+        /// <returns></returns>
+        public bool ModifierDocument(string Id, string Titre, string Image, string IdGenre, string IdPublic, string IdRayon)
+        {
+            return access.ModifierDocument(Id, Titre, Image, IdGenre, IdPublic, IdRayon);
+        }
+
+        /// <summary>
+        /// Supprime un document dans la bdd
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public bool SupprimerDocument(string Id)
+        {
+            return access.SupprimerDocument(Id);
+        }
+
+        /// <summary>
+        /// Crée un livre dans la bdd
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <param name="Isbn"></param>
+        /// <param name="Auteur"></param>
+        /// <param name="Collection"></param>
+        /// <returns></returns>
+        public bool CreerLivre(string Id, string Isbn, string Auteur, string Collection)
+        {
+            return access.CreerLivre(Id, Isbn, Auteur, Collection);
+        }
+
+        /// <summary>
+        /// Modifie un livre dans la bdd
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <param name="Isbn"></param>
+        /// <param name="Auteur"></param>
+        /// <param name="Collection"></param>
+        /// <returns></returns>
+        public bool ModifierLivre(string Id, string Isbn, string Auteur, string Collection)
+        {
+            return access.ModifierLivre(Id, Isbn, Auteur, Collection);
+        }
+
+        /// <summary>
+        /// Suppression d'un livre en bdd
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public bool SupprimerLivre(string Id)
+        {
+            return access.SupprimerLivre(Id);
+        }
+
+    }
+}
